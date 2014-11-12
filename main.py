572ecb7724bd047003457d533ef2e2731c8c77e5
03florent <florentchansecwai@gmail.com>
2014-11-12 06:44:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #### Calculatrice Binaire :D ###
 a=input("entrez le premier nombre binaire")
-b=input("entre le second nombre binaire")
 s=input("Entrer le signe de l'opération")
+b=input("entre le second nombre binaire")
 e=0
 def f(x):      #c'est la fonction pour convertir decimale binaire :)#
     xdec=int(0)
