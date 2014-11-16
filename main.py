f5cf7b1b3ffb32bf51c55086b84f4a953ffef86d
pilotdynamo <kevin.chan@eleve.egd.mg>
2014-11-16 19:10:05 +0100

    Update main.py
    la consigne stipulait uniquement les nombres entiers positifs avec comme opérations les additions et multiplications...

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#### Calculatrice Binaire :D ###
-a=input("entrez le premier nombre binaire: ")
-s=input("Entrer le signe de l'opération: ")
-b=input("entre le second nombre binaire: ")
+#### Calculatrice Binaire uniquement pour les entiers positifs avec comme opération les addition et multiplication :D ###
+a=input("entrez le premier nombre binaire entier et positif: ")
+s=input("Entrer le signe de l'opération (soit + soit *): ")
+b=input("entre le second nombre binaire entier et positif: ")
 e=0
 def f(x):      #c'est la fonction pour convertir decimale binaire :)#
     xdec=int(0)
@@ -13,23 +13,11 @@ def f(x):      #c'est la fonction pour convertir decimale binaire :)#
         else:
             r=r-1
     return(xdec)
-if s == "+":
-    e = f(a)+f(b)
-    print(a,s,b,"=",bin(e)[2:])#pour afficher le résultat sans le 0b
-elif s == "-":
-    e= f(a)-f(b)
-    print(a,s,b,"= -",bin(e)[3:])
-elif s== "*":
-    e= f(a)*f(b)
+if s=="+" or s=="*":
+    if "+" in s:
+        e=f(a)+f(b)
+    if "*" in s:
+        e=f(a)*f(b)
     print(a,s,b,"=",bin(e)[2:])
-elif s== "/":
-    e= f(a)/f(b)
-    if type(e)!= int:
-        print("Cette calculatrice ne prend pas en charge les nombres décimal")
-    print(a,s,b,"=",bin(int(e))[2:])# pyhton ne prend pas les type float donc il faut préciser le int
-elif s!="+" and s!="-" and s!="*" and s!="/":
-    print("veuillez recommencer l'opération")
-elif "," in a or b:
-     print("Cette calculatrice ne prend pas en charge les nombres décimal")
-
-
+else:
+    print("veuillez recommencer, l'opération n'a pas été reconue")
